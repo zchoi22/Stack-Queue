d10b2991e03ab164dec6f3d7d7e4f1189c9cdf31
Zion Choi <zchoi22@kentdenver.org>
2022-01-30 17:01:08 -0700

    added comemnts

--- Stack_Queue.py
+++ Stack_Queue.py
@@ -6,6 +6,8 @@ class Stack_Queue:
         self.stack_top = Stack()
         self.stack_bottom = Stack()
 
+    #dequeue moves all elements in a stack to the other one,
+    #pops the top elements, and moves then back to imitate a first in last out
     def dequeue(self):
         while self.stack_top.isEmpty() == False:
             self.stack_bottom.push(self.stack_top.pop())
@@ -13,9 +15,11 @@ class Stack_Queue:
         while self.stack.bottom.isEmpty() == False:
             self.stack_top.push(self.stack_top.pop())
 
+    #relies on the top stack, simply pushes new node on top
     def enqueue(self, node):
         self.stack_top.push(node)
 
+    #again, relies on top stack, same for size and peek
     def isEmpty(self):
         return self.stack_top.isEmpty()
 
